c4078ead11e3740041c1410c94525a0039ecd05f
DARKROSS <andresarcangel00@gmail.com>
2025-08-08 00:19:18 -0500

    test integration

--- ms-account-movement/src/test/java/com/acampoverde/ms_account_movement/infraestructure/controller/AccountControllerIntegrationTest.java
+++ ms-account-movement/src/test/java/com/acampoverde/ms_account_movement/infraestructure/controller/AccountControllerIntegrationTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.mockito.Mock;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,7 +31,7 @@ class AccountControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private AccountHandler accountHandler;
 
     private ObjectMapper objectMapper;
